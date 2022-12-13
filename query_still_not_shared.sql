00068a25b1712da5b0bebd6146b759d0004283ac
TimoKuukka <46355@edu.raseko.fi>
2022-12-13 15:06:02 +0200

    added

---
+++ query_still_not_shared.sql
@@ -0,0 +1,6 @@
+-- Kysely, jolla selvitetään paljonko kaadetusta eläimestä on jäänyt jakamatta
+SELECT kaato.kaato_id,
+    ruhopaino - sum (maara) AS jakamatta
+FROM public.kaato INNER JOIN public.jakotapahtuma ON jakotapahtuma.kaato_id = kaato.kaato_id
+GROUP BY kaato.kaato_id
+
